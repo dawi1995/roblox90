9048608d966248c193d2bbcaf27973cd18e8e822
dawi1995 <dawidsonb95@gmail.com>
2025-02-04 18:43:38 +0100

    Update moveObject.lua

--- M4L3_Effect_and_respawn/moveObject.lua
+++ M4L3_Effect_and_respawn/moveObject.lua
@@ -4,13 +4,13 @@ local button = script.Parent
 local model = game.Workspace.NewKodlandCar
 -- ustawienie pozycji, w której chcemy, aby samochód się pojawił
 local targetPosition = Vector3.new(0, 0, 0)
---local targetOrientation = Vector3.new(0,0,0)
+local targetOrientation = Vector3.new(0,0,0)
 
 -- funkcja, która przesuwa obiekt samochodu
 local function moveModel()
     -- przesuwanie obiektu do pozycji, którą zdefiniowaliśmy powyżej
-    model:SetPrimaryPartCFrame(CFrame.new(targetPosition))
-    --model:SetPrimaryPartCFrame(CFrame.new(targetPosition) * CFrame.Angles(math.rad(targetOrientation.X), math.rad(targetOrientation.Y), math.rad(targetOrientation.Z)))
+    -- model:SetPrimaryPartCFrame(CFrame.new(targetPosition))
+    model:SetPrimaryPartCFrame(CFrame.new(targetPosition) * CFrame.Angles(math.rad(targetOrientation.X), math.rad(targetOrientation.Y), math.rad(targetOrientation.Z)))
 end
 
 -- funkcja ponownego pojawienia się będzie wywoływana za każdym razem, gdy gracz naciśnie przycisk
